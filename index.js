38dbf33c33eb27e7c8e30bbfb7d8153d0d8f9b42
protim1451 <partha.doict@gmail.com>
2024-05-15 23:30:30 +0600

    changes for hosting

--- index.js
+++ index.js
@@ -5,7 +5,16 @@ const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.use(cors());
+app.use(
+    cors({
+      origin: [
+        "http://localhost:5173",
+        "https://b9a11-client-side-protim1451.firebaseapp.com",
+        "https://b9a11-client-side-protim1451.web.app",
+      ],
+      credentials: true,
+    })
+  );
 app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.edgm8kl.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
